b990652d8a33b2445ca6592d980ad25edc5d7c2d
salahulbain <salahul.bain@gmail.com>
2022-06-30 11:49:45 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Absensi USM V2'),
+      ),
+      body: const WebView(
+        initialUrl: 'https://absen.serambimekkah.ac.id/login',
+      ),
+    );
+  }
+}
